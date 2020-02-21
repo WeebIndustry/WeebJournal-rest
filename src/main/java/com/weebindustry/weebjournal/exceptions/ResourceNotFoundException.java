67ccf8f32379fd2bec6bedaab2b02ce9c44c1804
HarryCoder <vucaominh1408@gmail.com>
2020-02-21 13:35:47 +0700

    Created ResourceNotFoundException

--- src/main/java/com/weebindustry/weebjournal/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/weebindustry/weebjournal/exceptions/ResourceNotFoundException.java
@@ -3,30 +3,14 @@ package com.weebindustry.weebjournal.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import lombok.Data;
+
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
+@Data
 public class ResourceNotFoundException extends RuntimeException {
     private static final long serialVersionUID = -6564141208065599750L;
 
-    private String resourceName;
-    private String fieldName;
-    private Object fieldValue;
-
-    public ResourceNotFoundException( String resourceName, String fieldName, Object fieldValue) {
-        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
-        this.resourceName = resourceName;
-        this.fieldName = fieldName;
-        this.fieldValue = fieldValue;
-    }
-
-    public String getResourceName() {
-        return resourceName;
-    }
-
-    public String getFieldName() {
-        return fieldName;
-    }
-
-    public Object getFieldValue() {
-        return fieldValue;
+    public ResourceNotFoundException(String message) {
+        super(message);
     }
 }
