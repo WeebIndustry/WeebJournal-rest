ad5bd6437bef65d84f2bb2dddbdc5c1c26104b13
HarryCoder <vucaominh1408@gmail.com>
2020-02-21 14:54:22 +0700

    Added 2 DTOs for Post Model

---
+++ src/main/java/com/weebindustry/weebjournal/dtos/posts/PostCreatableDTO.java
@@ -0,0 +1,13 @@
+package com.weebindustry.weebjournal.dtos.posts;
+
+import lombok.*;
+
+
+@Data
+public class PostCreatableDTO {
+
+    private String title;
+
+    private String content;
+    
+}
---
+++ src/main/java/com/weebindustry/weebjournal/dtos/posts/PostUpdatableDTO.java
@@ -0,0 +1,12 @@
+package com.weebindustry.weebjournal.dtos.posts;
+
+import lombok.Data;
+
+@Data
+public class PostUpdatableDTO {
+    
+    private String title;
+
+    private String content;
+    
+}
