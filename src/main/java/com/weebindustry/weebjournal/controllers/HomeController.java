019d36639c1a6be5039ffa2701b98bb65c037f82
HarryCoder <vucaominh1408@gmail.com>
2020-03-08 22:33:24 +0700

    Added HomeController, TagController and an exception

---
+++ src/main/java/com/weebindustry/weebjournal/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.weebindustry.weebjournal.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("")
+    public String home() {
+        return "This is the WeebJournal";
+    }
+}
---
+++ src/main/java/com/weebindustry/weebjournal/controllers/TagController.java
@@ -0,0 +1,66 @@
+package com.weebindustry.weebjournal.controllers;
+
+import com.weebindustry.weebjournal.dtos.tags.TagCreatableDTO;
+import com.weebindustry.weebjournal.dtos.tags.TagUpdatableDTO;
+import com.weebindustry.weebjournal.dtos.users.UserCreatableDTO;
+import com.weebindustry.weebjournal.dtos.users.UserUpdatableDTO;
+import com.weebindustry.weebjournal.exceptions.RequestParamValueNotDefinedException;
+import com.weebindustry.weebjournal.exceptions.ResourceNotFoundException;
+import com.weebindustry.weebjournal.models.Tag;
+import com.weebindustry.weebjournal.models.User;
+import com.weebindustry.weebjournal.util.DTO;
+import com.weebindustry.weebjournal.util.HelperService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/")
+public class TagController {
+
+    private final HelperService<Tag> service;
+
+    public TagController(HelperService<Tag> service) {
+        this.service = service;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<?> getAllUsers(Pageable pageable, @RequestParam(required = false, defaultValue = "false") String hasPagable) {
+        if (hasPagable.equals("true")) {
+            return ResponseEntity.ok(service.findAll(pageable));
+        }
+        else if(hasPagable.equals("false")) {
+            return ResponseEntity.ok(service.list());
+        }
+        else {
+            throw new RequestParamValueNotDefinedException("hasPagable", hasPagable);
+        }
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tag> findById(@PathVariable(value = "id") Long id) throws ResourceNotFoundException {
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+
+    @PostMapping("/")
+    public ResponseEntity<Tag> createTag(@Valid @RequestBody @DTO(TagCreatableDTO.class) Tag tag) {
+        return ResponseEntity.ok(service.create(tag));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Tag> updateTag(@PathVariable Long id, @Valid @RequestBody @DTO(TagUpdatableDTO.class) Tag tag) {
+        return ResponseEntity.ok(service.update(id, tag));
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteTag(@PathVariable(value = "id") Long id) {
+        service.delete(id);
+        return ResponseEntity.ok().build();
+    }
+}
