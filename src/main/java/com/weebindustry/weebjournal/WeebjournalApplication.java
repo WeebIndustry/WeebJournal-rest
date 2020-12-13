4027339a0758d5c458d87ec234196f837a937c9d
Aragami1408 <vucaominh1408@gmail.com>
2020-12-13 16:59:46 +0700

    Enable FeignClient and Import SwaggerConfig

--- src/main/java/com/weebindustry/weebjournal/WeebjournalApplication.java
+++ src/main/java/com/weebindustry/weebjournal/WeebjournalApplication.java
@@ -1,11 +1,17 @@
 package com.weebindustry.weebjournal;
 
+import com.weebindustry.weebjournal.config.SwaggerConfig;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableFeignClients
+@Import(SwaggerConfig.class)
 public class WeebjournalApplication {
 
 	public static void main(String[] args) {
