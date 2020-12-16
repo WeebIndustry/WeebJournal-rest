62ffecd850f36cc402c2d0f90c72d3fe211d236a
Aragami1408 <vucaominh1408@gmail.com>
2020-12-16 08:50:00 +0700

    Add @Bean annotation

--- src/main/java/com/weebindustry/weebjournal/config/SwaggerConfig.java
+++ src/main/java/com/weebindustry/weebjournal/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.weebindustry.weebjournal.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -13,6 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    @Bean
     public Docket weebjournalApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
